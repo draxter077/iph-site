400b0a1ba75fdc70f32bea30053d449dcbafd86d
draxter-07 <philippe.idalgoprestes@gmail.com>
2023-12-01 16:46:25 -0300

    oi

--- src/pages/startPage/style.js
+++ src/pages/startPage/style.js
@@ -103,7 +103,7 @@ export const AcessContainer = styled.div`
         width: fit-content;
         margin: 30px 0px 0px 0px;
         opacity: ${atr => atr.disabled ? "0" : "1"};
-        transition: opacity ${fastTransition};
+        transition: opacity ${fastTransition}, color ${fastTransition};
     }
 
     div{
