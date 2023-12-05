cbbaba44f8bae3c7c63f93c8f9acf863b0891a2c
Philippe <philippe.idalgoprestes@gmail.com>
2023-12-05 15:48:54 -0300

    hm

--- src/pages/homePage/parts/console/Section.jsx
+++ src/pages/homePage/parts/console/Section.jsx
@@ -1,19 +1,28 @@
 import { SectionDiv } from "./style.js"
+import { API } from "../../../../variablesValues.js"
 
 export function ConsoleLine(atr){
     async function send(e){
         let line = e.target.parentElement.children[0].value
-        let commands = line.split("-")
+        let commands = line.split("+")
+        const commandType = commands[0]
+        const commandValue = Number(commands[1])
+        const date = {day: Number(commands[2]), month: Number(commands[3]), year: Number(commands[4])}
 
-        if(commands[0] == "revenue"){
-            await atr.axios.post(API + "/revenueReport", {revenue: commands[1]})
+        if(commandType == "revenue"){
+            await atr.axios.post(API + "/revenueReport", {revenue: commandValue, date: date})
                 .then(resposta => {alert(resposta.status)})
+                .catch(response => {alert(response.response)})
+        }
+        else if(commandType == "darf"){
+            await atr.axios.post(API + "/darfValue", date)
+                .then(resposta => {alert(resposta.data.value)})
                 .catch(response => {alert(response.response)})
         }
     }
     return(
         <SectionDiv>
-            <input placeholder="Digite o comando"></input>
+            <input placeholder="Digite o comando (+ to args)"></input>
             <button onClick={(e) => send(e)}>Enviar</button>
         </SectionDiv>
     )
--- src/pages/homePage/parts/console/style.js
+++ src/pages/homePage/parts/console/style.js
@@ -1,8 +1,15 @@
 import styled from "styled-components"
+import { fontSizeSmall } from "../../../../visualValues"
 
 export const SectionDiv = styled.div`
     display: none;
     flex-direction: row;
     align-items: flex-start;
     width: 100%;
+
+    margin: 10px 0px 0px 0px;
+
+    input{
+        font-size: ${fontSizeSmall};
+    }
 `
