62f31d631fd0c48fe0f9fe83b55c2e4d25aac9d0
Philippe <philippe.idalgoprestes@gmail.com>
2023-12-07 11:32:32 -0300

    op

--- src/pages/homePage/parts/console/Section.jsx
+++ src/pages/homePage/parts/console/Section.jsx
@@ -4,21 +4,9 @@ import { API } from "../../../../variablesValues.js"
 export function ConsoleLine(atr){
     async function send(e){
         let line = e.target.parentElement.children[0].value
-        let commands = line.split("+")
-        const commandType = commands[0]
-        const commandValue = Number(commands[1])
-        const date = {day: Number(commands[2]), month: Number(commands[3]), year: Number(commands[4])}
-
-        if(commandType == "revenue"){
-            await atr.axios.post(API + "/revenueReport", {revenue: commandValue, date: date})
-                .then(resposta => {alert(resposta.status)})
-                .catch(response => {alert(response.response)})
-        }
-        else if(commandType == "darf"){
-            await atr.axios.post(API + "/darfValue", date)
-                .then(resposta => {alert(resposta.data.value)})
-                .catch(response => {alert(response.response)})
-        }
+        await atr.axios.post(API + "/console", {command: line})
+            .then(resposta => {alert(resposta.status)})
+            .catch(response => {alert(response.response)})
     }
     return(
         <SectionDiv>
