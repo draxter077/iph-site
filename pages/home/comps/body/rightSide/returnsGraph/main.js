09535fb2c13cce611aa8bf518f322d66422959bf
Philippe <philippe.idalgoprestes@gmail.com>
2024-07-27 00:43:53 -0300

    lkast touchs

--- pages/home/comps/body/rightSide/returnsGraph/main.js
+++ pages/home/comps/body/rightSide/returnsGraph/main.js
@@ -1,8 +1,26 @@
 export default function returnsGraph(){
-    const values = [{date: "07/2023: 0%", y: "50%"}, {date: "08/2023: -5%", y: "72.5%"}]
+    const values = [{date: "07/2023: 0%", y: "50%"}, 
+                    {date: "08/2023: -5%", y: "72.5%"},
+                    {date: "09/2023: 10%", y: "5%"},
+                    {date: "10/2023: 0%", y: "50%"},
+                    {date: "11/2023: 5%", y: "27.5%"}
+                ]
     return(`
         <div class="homeBodyReturnsGraphDiv">
             <svg>
+                <line x1="5%" x2="95%" y1="95%" y2="95%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
+                <line x1="5%" x2="95%" y1="72.5%" y2="72.5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
+                <line x1="5%" x2="95%" y1="50%" y2="50%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
+                <line x1="5%" x2="95%" y1="27.5%" y2="27.5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
+                <line x1="5%" x2="95%" y1="5%" y2="5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
+
+                <text x="0%" y="7.5%">10%</text>
+                <text x="0%" y="30%">5%</text>
+                <text x="0%" y="52.5%">0%</text>
+                <text x="0%" y="75%">-5%</text>
+                <text x="0%" y="97.5%">-10%</text>
+                <line x1="5%" x2="95%" y1="50%" y2="50%" stroke-width="1" stroke="rgb(255, 0, 200)"></line>
+
                 <g>
                     ${values.map(d => {
                         return(
@@ -18,12 +36,6 @@ export default function returnsGraph(){
                         `)
                     })}
                 </g>
-                <text x="0%" y="52.5%">0%</text>
-                <line x1="5%" x2="95%" y1="95%" y2="95%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
-                <line x1="5%" x2="95%" y1="72.5%" y2="72.5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
-                <line x1="5%" x2="95%" y1="50%" y2="50%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
-                <line x1="5%" x2="95%" y1="27.5%" y2="27.5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
-                <line x1="5%" x2="95%" y1="5%" y2="5%" stroke-width="1" stroke="rgb(0, 0, 0, 0.2)"></line>
             </svg>
         </div>    
     `)
