73db177cac8fc287d14dcd54767f7b29daf727f2
Philippe <philippe.idalgoprestes@gmail.com>
2024-10-01 14:49:16 -0300

    accountTotal change

--- pages/home/body/leftSide/accountTotal/main.js
+++ pages/home/body/leftSide/accountTotal/main.js
@@ -1,3 +1,37 @@
+function returnR$(n){
+    let parts = n.toString().replaceAll(".", ",").split(",")
+
+    let decimals
+    if(parts.length > 1){
+        decimals = parts[1]
+        if(decimals.length == 1){
+            decimals += "0"
+        }
+        else if(decimals.length > 2){
+            decimals = Math.round(Number(decimals)/(10*(decimals.length - 2))).toString()
+        }
+    }
+    else{
+        decimals = "00"
+    }
+
+    let digits = ""
+    if(parts[0].length > 3){
+        let m = parts[0].length%3
+        for(let i = 0; i < parts[0].length; i++){
+            if((i - m)%3 == 0 && (i != 0)){
+                digits += "."
+            }
+            digits += parts[0][i]
+        }
+    }
+    else{
+        digits = parts[0]
+    }
+
+    return("R$ " + digits + "," + decimals)
+}
+
 export default function accountTotal(data){
     const accountTotal = document.createElement("div")
     accountTotal.className = "homeBodyLeftSideAccountTotal"
@@ -9,7 +43,7 @@ export default function accountTotal(data){
         const contents = document.createElement("div")
         contents.className = "homeBodyLeftSideAccountTotalContents"
             const value = document.createElement("div")
-            value.innerHTML = "R$ " + data.capital.toString().replaceAll(".", ",")
+            value.innerHTML = returnR$(data.capital)
             contents.appendChild(value)
 
             const rent = document.createElement("div")
