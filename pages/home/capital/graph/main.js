691d2299d3023c8425be622b7c9af18f92e174bc
philippe <philippe.idalgoprestes@gmail.com>
2025-05-30 19:57:14 -0300

    v12

--- pages/home/capital/graph/main.js
+++ pages/home/capital/graph/main.js
@@ -20,7 +20,7 @@ export default function graph(rents){
         }`
 
     const graph = cE("div", style)
-    if(rents == []){
+    if(rents.length == 0){
         rents = [
             {date:"00/00/00", value:0},
             {date:"00/00/00", value:0},
