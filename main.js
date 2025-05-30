40405f91ff49a675e4b57e5f57fb8de71aa9e0ab
philippe <philippe.idalgoprestes@gmail.com>
2025-05-30 18:20:46 -0300

    api

--- main.js
+++ main.js
@@ -156,7 +156,7 @@ window.construct = function construct(p){
     }
 }
 
-window.apiURL = ""
+window.apiURL = "https://ace-chimp-merry.ngrok-free.app/iph"
 
 axios.defaults.headers.common["ngrok-skip-browser-warning"] = "69420"
 document.getElementsByTagName("head")[0].appendChild(document.createElement("style"))
