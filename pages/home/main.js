afdda992ede39b8d726f39a8aa7cda5d3c4d5e27
philippe <philippe.idalgoprestes@gmail.com>
2025-05-30 18:55:50 -0300

    v5

--- pages/home/main.js
+++ pages/home/main.js
@@ -4,7 +4,7 @@ import info from "./info/main.js"
 import capital from "./capital/main.js"
 import movs from "./movs/main.js"
 
-export default function home(){
+export default function home(data){
     let style = `
         {
             display:flex;
@@ -18,27 +18,6 @@ export default function home(){
         }`
 
     const home = cE("div", style)
-
-    let data = {
-        name:"Name Surname1 Surname2",
-        email:"name@email.com",
-        pix:"algumaChave",
-        movs:[{date:"01/01/2025", type:"Depósito", value:1250, status:1},
-                {date:"01/01/2025", type:"Retirada", value:-1000, status:1},
-                {date:"31/01/2025", type:"Lucro", value:50, status:1},
-        
-                {date:"01/02/2025", type:"Retirada", value:-300, status:1},
-                {date:"01/02/2025", type:"Depósito", value:1250, status:1},
-                {date:"01/02/2025", type:"Retirada", value:-1000, status:1},
-                {date:"31/02/2025", type:"Lucro", value:200, status:1},
-        
-                {date:"01/03/2025", type:"Retirada", value:-300, status:1},
-                {date:"01/03/2025", type:"Depósito", value:1250, status:1},
-                {date:"01/03/2025", type:"Retirada", value:-1000, status:1},
-                {date:"31/03/2025", type:"Lucro", value:200, status:1}
-            ]
-    }
-
     home.appendChild(head())
     home.appendChild(config(data.name,data.email,data.pix))
     home.appendChild(info())
