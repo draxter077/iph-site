4879c5a6d1a65c615afbce66db65702e81352d95
philippe <philippe.idalgoprestes@gmail.com>
2025-07-31 13:09:54 -0300

    change qr copy

--- pages/home/capital/value/buttons/line/value/depositWindow/copy/main.js
+++ pages/home/capital/value/buttons/line/value/depositWindow/copy/main.js
@@ -12,6 +12,17 @@ export default function copy(brCode){
 
     const copy = cE("img", style)
     copy.src = "./assets/copy.png"
-    copy.addEventListener("click", () => navigator.clipboard.writeText(brCode))
+    copy.addEventListener(
+        "click",
+        function a(){
+            let text = document.createElement("textarea");
+            text.value = text;
+            document.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            document.execCommand('copy');
+            document.removeChild(text)
+        }
+    )
     return(copy)
 }
